63e05d3f2b287aa17007032d6174bedb4cfcd80b
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2022-12-13 22:12:43 +0300

    Add files via upload

---
+++ GameUİSnonWorld.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string mcuiloader,garbageloader,abc;
+  string garbageloaderfinder="İs LOADİNG";
+
+  cout<<"Lmao Did you really thought this is real. Microsoft will fuck
+myass"<<endl;
+
+        cin>>mcuiloader;
+
+  if(mcuiloader==garbageloaderfinder)
+    cout<<"Uİ is failed to load"<<endl;
+  else
+    cin>>abc;
+
+  if(abc=="Minecraft is failed")
+    cout<<"Please shutdown minecraft"<<endl;
+
+
+
+  return 0;
+}
